47d027e056863d07e81edb9231bd332295ecc715
froide_ch.ono <chiharu-ono@froide.co.jp>
2022-06-26 14:26:46 +0900

    かんせい

--- common/js/base.js
+++ common/js/base.js
@@ -1,15 +1,16 @@
 window.onload = function () {
   var subHeader = document.getElementById("subHeaderArea");
+  var mainHeader = document.getElementById("mainHeaderArea");
   var about = document.getElementById("item__about");
   var solutions = document.getElementById("item__solutions");
   var ir = document.getElementById("item__ir");
   var sustainability = document.getElementById("item__sustainability");
   var pressRoom = document.getElementById("item__press-room");
-
-
+  var scroll = document.getElementById("scroll");
 
   about.addEventListener("mouseover", function () {
     subHeader.style.display = 'block';
+    mainHeader.style.backgroundColor = '#fff';
     document.getElementById("list__about").style.display = 'block';
     document.getElementById("list__solutions").style.display = 'none';
     document.getElementById("list__ir").style.display = 'none';
@@ -19,6 +20,7 @@ window.onload = function () {
   
   solutions.addEventListener("mouseover", function () {
     subHeader.style.display = 'block';
+    mainHeader.style.backgroundColor = '#fff';
     document.getElementById("list__about").style.display = 'none';
     document.getElementById("list__solutions").style.display = 'block';
     document.getElementById("list__ir").style.display = 'none';
@@ -28,6 +30,7 @@ window.onload = function () {
   
   ir.addEventListener("mouseover", function () {
     subHeader.style.display = 'block';
+    mainHeader.style.backgroundColor = '#fff';
     document.getElementById("list__about").style.display = 'none';
     document.getElementById("list__solutions").style.display = 'none';
     document.getElementById("list__ir").style.display = 'block';
@@ -37,6 +40,7 @@ window.onload = function () {
 
   sustainability.addEventListener("mouseover", function () {
     subHeader.style.display = 'block';
+    mainHeader.style.backgroundColor = '#fff';
     document.getElementById("list__about").style.display = 'none';
     document.getElementById("list__solutions").style.display = 'none';
     document.getElementById("list__ir").style.display = 'none';
@@ -46,6 +50,7 @@ window.onload = function () {
 
   pressRoom.addEventListener("mouseover", function () {
     subHeader.style.display = 'block';
+    mainHeader.style.backgroundColor = '#fff';
     document.getElementById("list__about").style.display = 'none';
     document.getElementById("list__solutions").style.display = 'none';
     document.getElementById("list__ir").style.display = 'none';
@@ -55,10 +60,18 @@ window.onload = function () {
 
 
   subHeader.addEventListener("mouseover", function () {
-    document.getElementById("subHeaderArea").style.display = 'block';
+    subHeader.style.display = 'block';
+    mainHeader.style.backgroundColor = '#fff';
   }, false);
 
   subHeader.addEventListener("mouseout", function () {
-    document.getElementById("subHeaderArea").style.display = 'none';
+    subHeader.style.display = 'none';
+    mainHeader.style.backgroundColor = 'rgba(255, 255, 255, 0)';
   }, false);
+
+  scroll.onclick = function() {
+    window.scroll({top: window.innerHeight, behavior: 'smooth'});
+  };
+
+
 }
